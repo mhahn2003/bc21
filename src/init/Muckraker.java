b44f6548b00f395ccaa6d9c904f81a3e44b8de83
Michael Hahn <skipiano@gmail.com>
2021-01-06 15:27:07 -0800

    muckraker does stuff now

--- src/init/Muckraker.java
+++ src/init/Muckraker.java
@@ -16,33 +16,51 @@ public class Muckraker extends Robot {
         super.takeTurn();
         Team enemy = rc.getTeam().opponent();
         int actionRadius = rc.getType().actionRadiusSquared;
+        int closestPoliticianDist = 100000;
+        MapLocation closestPolitician = null;
+        int closestSlandererDist = 100000;
+        MapLocation closestSlanderer = null;
+        int maxSlanderer = -1;
         for (RobotInfo robot : rc.senseNearbyRobots(actionRadius, enemy)) {
             if (robot.type.canBeExposed()) {
-                // It's a slanderer... go get them!
-                if (rc.canExpose(robot.location)) {
-                    System.out.println("e x p o s e d");
-                    rc.expose(robot.location);
-                    return;
+                int dist = rc.getLocation().distanceSquaredTo(robot.getLocation());
+                if (dist <= closestSlandererDist) {
+                    closestSlandererDist = dist;
+                    closestSlanderer = robot.location;
+                }
+                if (dist <= RobotType.MUCKRAKER.actionRadiusSquared) {
+                    maxSlanderer = Math.max(maxSlanderer, robot.getInfluence());
+                }
+            }
+            if (robot.type == RobotType.POLITICIAN) {
+                int dist = rc.getLocation().distanceSquaredTo(robot.getLocation());
+                if (dist > 2 && dist <= closestPoliticianDist) {
+                    closestPoliticianDist = dist;
+                    closestPolitician = robot.location;
                 }
             }
         }
-        if (tryMove(randomDirection()))
-            System.out.println("I moved!");
-    }
-
-    static boolean tryMove(Direction dir) throws GameActionException {
-        System.out.println("I am trying to move " + dir + "; " + rc.isReady() + " " + rc.getCooldownTurns() + " " + rc.canMove(dir));
-        if (rc.canMove(dir)) {
-            rc.move(dir);
-            return true;
-        } else return false;
-    }
-
-    static Direction randomDirection() {
-        return directions[(int) (Math.random() * directions.length)];
+        // expose the max slanderer in range
+        if (maxSlanderer != -1) {
+            for (RobotInfo robot : rc.senseNearbyRobots(actionRadius, enemy)) {
+                if (robot.type.canBeExposed() & rc.canExpose(robot.location)) {
+                    rc.expose(robot.location);
+                }
+            }
+        }
+        // move to the closest slanderer if not
+        if (closestSlandererDist != 100000) {
+            nav.goTo(closestSlanderer);
+        } else {
+            // else move to the nearest politician not adjacent
+            if (closestPolitician != null) nav.goTo(closestPolitician);
+            // otherwise wander
+            else wander();
+        }
     }
 
     // wander around
+    // TODO: what if you're already at a corner/side and you want to explore more
     public void wander() throws GameActionException {
         nav.goTo(wandLoc);
     }
