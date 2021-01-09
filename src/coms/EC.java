b35c1f93ad9738c8cd01f8be360b30e40bd91141
Michael Hahn <skipiano@gmail.com>
2021-01-09 14:01:45 -0800

    basic ec building logic

--- src/coms/EC.java
+++ src/coms/EC.java
@@ -5,6 +5,8 @@ import battlecode.common.GameActionException;
 import battlecode.common.RobotController;
 import battlecode.common.RobotType;
 
+import static coms.RobotPlayer.turnCount;
+
 public class EC extends Robot {
 
     public EC(RobotController rc) {
@@ -13,8 +15,33 @@ public class EC extends Robot {
 
     public void takeTurn() throws GameActionException {
         super.takeTurn();
-        RobotType toBuild = randomSpawnableRobotType();
-        int influence = 50;
+        if (turnCount == 1) {
+            // build a slanderer
+            build(RobotType.SLANDERER, 150);
+        }
+        if (turnCount < 60) {
+            if (rc.getInfluence() < 150) {
+                build(RobotType.MUCKRAKER, 1);
+            } else {
+                build(RobotType.POLITICIAN, rc.getInfluence());
+            }
+        } else {
+            int rand = (int) (Math.random() * 4);
+            if (rand == 0) {
+                build(RobotType.POLITICIAN, 15);
+            }
+            else if (rand == 3) {
+                build(RobotType.MUCKRAKER, 1);
+            }
+            else {
+                if (rc.getInfluence() > 150) {
+                    build(RobotType.POLITICIAN, rc.getInfluence());
+                }
+            }
+        }
+    }
+
+    public void build(RobotType toBuild, int influence) throws GameActionException {
         for (Direction dir : directions) {
             if (rc.canBuildRobot(toBuild, dir, influence)) {
                 rc.buildRobot(toBuild, dir, influence);
@@ -22,9 +49,5 @@ public class EC extends Robot {
                 break;
             }
         }
-    }
-
-    static RobotType randomSpawnableRobotType() {
-        return spawnableRobot[(int) (Math.random() * spawnableRobot.length)];
     }
 }
